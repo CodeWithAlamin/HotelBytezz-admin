e4fdcaa89ea10cf4862db9f4a905ae85b657eb58
CWA <codepapa360@gmail.com>
2026-04-20 14:54:08 +0600

    Remove hardcoded email and password from LoginForm

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,11 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("alamin@example.com");
-  const [password, setPassword] = useState("12345678");
+  // const [email, setEmail] = useState("alamin@example.com");
+  // const [password, setPassword] = useState("12345678");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
@@ -23,7 +26,7 @@ function LoginForm() {
           setEmail("");
           setPassword("");
         },
-      }
+      },
     );
   }
 
